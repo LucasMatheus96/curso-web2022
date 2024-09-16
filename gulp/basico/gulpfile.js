244e8db93e0dbc19b4b6f173f7a7fe67848c406f
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2024-09-16 11:34:16 -0300

    Ajustes Gulp

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,9 +1,29 @@
 const gulp = require('gulp')
 const series = gulp.series
+const {series, parallel} = require('gulp')
 
+const antes1= cb =>{
+    console.log('Tarefa antes 1')
+    return cb()
+}
+
+
+const antes2= cb =>{
+    console.log('Tarefa antes 2')
+    return cb()
+}
 function copiar(cb){
     console.log('Tarefa de copiar!')
     return cb()
 }
 
-module.exports.default = series(copiar)
+const fim= cb =>{
+    console.log('Tarefa FIM')
+    return cb()
+}
+
+module.exports.default = series(
+    parallel(antes1,antes2),
+    copiar,
+    fim
+)
