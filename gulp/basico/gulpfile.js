88748f79865cc25b1e22b48e24aecd91cfb5966b
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2024-11-11 22:04:12 -0300

    319_FundamentosGulp_02

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,29 +1,33 @@
-const gulp = require('gulp')
-const series = gulp.series
-const {series, parallel} = require('gulp')
+const gulp = require('gulp');
+const { series, parallel } = require('gulp');
 
-const antes1= cb =>{
-    console.log('Tarefa antes 1')
-    return cb()
+const antes1 = cb => {
+    console.log('Tarefa antes 1');
+    return cb();
 }
 
-
-const antes2= cb =>{
-    console.log('Tarefa antes 2')
-    return cb()
-}
-function copiar(cb){
-    console.log('Tarefa de copiar!')
-    return cb()
+const antes2 = cb => {
+    console.log('Tarefa antes 2');
+    return cb();
 }
 
-const fim= cb =>{
-    console.log('Tarefa FIM')
-    return cb()
+function copiar(cb) {
+    //  gulp.src(['pastaA/arquivo1.txt','pastaA/arquivo2.txt'])
+    //  .pipe(gulp.dest('pastaB'))
+    // //  .pipe(imagePelaMetade())
+    // //  .pipe(imageEmpretoEBranco())
+    gulp.src('pastA/**/*.txt')
+    .pipe(gulp.dest('pastB'))
+    return cb();
+}
+
+const fim = cb => {
+    console.log('Tarefa FIM');
+    return cb();
 }
 
 module.exports.default = series(
-    parallel(antes1,antes2),
+    parallel(antes1, antes2),
     copiar,
     fim
-)
+);
