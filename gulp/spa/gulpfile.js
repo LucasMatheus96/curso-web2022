f9c9c97c779029f152517fac07d6e3e28b7b0b6b
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2024-11-13 22:00:13 -0300

    329-Criando estrutura de build

---
+++ gulp/spa/gulpfile.js
@@ -0,0 +1,17 @@
+const {series, parallel} = require('gulp')
+const gulp = require("gulp")
+
+const { appHtml,appCSS, appJS, appIMG} = require('./gulpTasks/app')
+const { depsCSS, depsFonts} = require('./gulpTasks/deps')
+const { servidor,monitorarArquivos} = require('./gulpTasks/servidor')
+
+
+module.exports.default = 
+    series(
+        parallel(
+            series(appHtml,appCSS,appJS,appIMG),
+            series(depsCSS,depsFonts)
+        ),
+        servidor,
+        monitorarArquivos
+    )
